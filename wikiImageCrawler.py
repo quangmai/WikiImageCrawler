2a9812ad4f978a8994622a42446166cb5fe34ece
Yongjoo Park <pyongjoo@umich.edu>
2012-03-28 14:59:34 -0400

    regex done

--- wikiImageCrawler.py
+++ wikiImageCrawler.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python
 
+import urllib2
+import re
+
+url_prefix = "http://en.wikipedia.org/wiki/"
+keyword = "Alabama"
+url = url_prefix + keyword
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+infile = opener.open(url)
+page = infile.read()
+
+match = re.findall(r"<img.*?src=[\"'](.+?)[\"']", page)
+
+print "<eecs485_pngs>"
+
+for m in match:
+	xmlImagePrefix = "<eecs485_png_url>"
+	print "http:" + m
+	xmlImageSuffix = "</eecs485_png_url>"
+
+print "</eecs485_pngs>"
+
 
